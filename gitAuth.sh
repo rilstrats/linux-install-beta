dc5a5ad13650e0eb12937b738a83237b520915ef
Riley Stratton <riley.s.stratton@outlook.com>
2022-03-16 23:06:28 -0600

    added gitAuth

---
+++ gitAuth.sh
@@ -0,0 +1,4 @@
+sudo pacman -S libgnome-keyring
+cd /usr/share/git/credential/gnome-keyring
+sudo make
+git config --global credential.helper /usr/share/git/credential/gnome-keyring/git-credential-gnome-keyring
