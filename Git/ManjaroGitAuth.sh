84a9ba143ba64f1cd6e8d3eab876d574fe17302e
Riley Stratton <riley.s.stratton@outlook.com>
2022-03-21 12:25:25 -0600

    more scripts

--- Git/ManjaroGitAuth.sh
+++ Git/ManjaroGitAuth.sh
@@ -1,8 +1,6 @@
 sudo pacman -S libgnome-keyring
 
-# The below line should work. If it doesn't, comment it out and uncomment the two lines below it.
-sudo make /usr/share/git/credential/gnome-keyring
-# cd /usr/share/git/credential/gnome-keyring
-# sudo make
+cd /usr/share/git/credential/gnome-keyring
+sudo make
 
 git config --global credential.helper /usr/share/git/credential/gnome-keyring/git-credential-gnome-keyring
---
+++ Git/UbuntuGitAuth.sh
@@ -0,0 +1,4 @@
+sudo apt install gnome-keyring
+cd /usr/share/doc/git/contrib/credential/gnome-keyring
+sudo make
+git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
